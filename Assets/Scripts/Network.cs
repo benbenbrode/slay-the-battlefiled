4b5f79f70847af2fa7c422ad4919fd2bcc4eae1e
ben <ckhs1205@gamil.com>
2024-10-21 20:47:37 +0900

    .gitattributes추가
    폰트깨져서 추가함

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using ExitGames.Client.Photon; // 커스텀 룸 속성 사용을 위한 네임스페이스
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,9 +8,10 @@ using UnityEngine.SceneManagement;
 public class Network : MonoBehaviourPunCallbacks
 {
     public static Network Instance;
-    public int type = 0;
-    public int type2 = 0;
-    public List<string> deck;
+    public int type = 0; // 내 직업
+    public int type2 = 0; // 상대 직업
+    public List<string> deck; // 내 덱
+
     private void Awake()
     {
         Debug.Log("NetworkManager Awake");
@@ -49,30 +51,44 @@ public class Network : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log("Joined Lobby");
-        TryJoinOrCreateRoom("game");
+        TryJoinOrCreateRoom();
     }
 
-    private void TryJoinOrCreateRoom(string roomName)
+    private void TryJoinOrCreateRoom()
     {
-        Debug.Log("Trying to join or create room: " + roomName);
+        Debug.Log("Trying to join a random room...");
 
-        // 방을 찾고 방이 존재하면 참가, 아니면 새로운 방을 생성
-        PhotonNetwork.JoinRandomRoom(null, 2);
+        // 커스텀 룸 속성을 설정해서 인원이 2명이 아닌 방을 찾도록 지정
+        Hashtable expectedCustomRoomProperties = new Hashtable() { { "IsFull", false } };
+
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 2);
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("No existing room found. Creating a new room.");
 
-        // 방이 존재하지 않으면 새로 생성
+        // 무작위로 방 이름을 생성
+        string roomName = GenerateRandomRoomName();
+
+        // 방이 존재하지 않으면 새로 생성, 커스텀 속성 추가
         RoomOptions roomOptions = new RoomOptions()
         {
             IsVisible = true,
             IsOpen = true,
-            MaxPlayers = 2
+            MaxPlayers = 2,
+            CustomRoomProperties = new Hashtable() { { "IsFull", false } }, // 방이 가득 차지 않았음을 나타내는 커스텀 속성
+            CustomRoomPropertiesForLobby = new string[] { "IsFull" } // 이 속성을 로비에서 검색할 수 있도록 등록
         };
 
-        PhotonNetwork.CreateRoom("game", roomOptions, TypedLobby.Default);
+        PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
+        Debug.Log("Created Room with Name: " + roomName);
+    }
+
+    // 무작위로 방 이름을 생성하는 함수
+    private string GenerateRandomRoomName()
+    {
+        return "Room_" + System.Guid.NewGuid().ToString("N"); // 고유한 문자열을 생성
     }
 
     public override void OnJoinedRoom()
@@ -96,44 +112,32 @@ public class Network : MonoBehaviourPunCallbacks
 
         if (PhotonNetwork.CurrentRoom.PlayerCount >= 2)
         {
-            Debug.Log("방에 들어왔음");
             if (!PhotonNetwork.IsMasterClient)
             {
                 Debug.Log("Not the master client, flipping camera view.");
-                //FlipCameraView();
             }
         }
     }
 
-    //private void FlipCameraView()
-    //{
-    //    Camera camera = Camera.main;
-    //    if (camera != null)
-    //    {
-    //        Matrix4x4 matrix = camera.projectionMatrix;
-    //        matrix.m01 = -matrix.m01; // Y축 반전
-    //        matrix.m11 = -matrix.m11; // Y축 반전
-    //        camera.projectionMatrix = matrix;
-    //    }
-    //    else
-    //    {
-    //        Debug.LogWarning("Main camera not found.");
-    //    }
-    //}
-
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         Debug.Log("A new player has entered the room: " + newPlayer.NickName);
+
         if (PhotonNetwork.CurrentRoom.PlayerCount >= 2)
         {
-
-            CheckPlayerCountAndSpawn();
+            // 커스텀 속성 업데이트: 방이 가득 찼음을 표시
+            PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable() { { "IsFull", true } });
         }
+
+        CheckPlayerCountAndSpawn();
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         Debug.Log("A player has left the room: " + otherPlayer.NickName);
+
+        // 플레이어가 방을 나가면 다시 방을 "가득 차지 않음"으로 표시
+        PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable() { { "IsFull", false } });
     }
 
     private void OnDestroy()
